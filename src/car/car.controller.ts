f3a6cb805bdb0cb460d15cdf8f3233172bc42ce2
Roie.Marzini <roie.marzini@leverate.com>
2022-07-10 13:02:16 +0300

    init

---
+++ src/car/car.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { query } from 'express';
+import { CarService } from './car.service';
+import { CarDto } from './car.tdo';
+
+@Controller('car')
+export class CarController {
+  constructor(private carService: CarService) {}
+
+  @Get()
+  public getCars() {
+    return this.carService.getCars();
+  }
+
+  @Post()
+  public postCar(@Body() car: CarDto) {
+    return this.carService.postCar(car);
+  }
+
+  @Get(':id')
+  public async getCarById(@Param('id') id: number) {
+    const result = await this.carService.getCarById(id);
+    return result;
+  }
+
+  @Delete(':id')
+  public async deleteCarById(@Param('id') id: number) {
+    return this.carService.deleteCarById(id);
+  }
+
+  @Put(':id')
+  public async putCarById(@Param('id') id: number, @Query() query) {
+    const propertyName = query.property_name;
+    const propertyValue = query.property_value;
+    return this.carService.putCarById(id, propertyName, propertyValue);
+  }
+}
